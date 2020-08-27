ba6fc732a5e47ef29569a8c99460c72db860ef64
Israel Menon <zezomenon@gmail.com>
2020-08-27 18:16:34 -0300

    Corrigindo ex2, e entregando ex3 comparacao entre tres nros

--- Introducao-Desenvolvimento-Web/Bloco_4/Dia_1/maiordedoisnros.js
+++ Introducao-Desenvolvimento-Web/Bloco_4/Dia_1/maiordedoisnros.js
@@ -3,6 +3,6 @@ let b = 10;
 
 if (a > b) {
   console.log(a + " é maior que " + b);
-} else {
-  console.log(a + " não é maior que " + b);
+} else if (b > a){
+  console.log(b + " é maior que " + a);
 }
