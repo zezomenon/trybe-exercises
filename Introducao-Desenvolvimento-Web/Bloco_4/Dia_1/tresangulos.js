a5812a7769ab81f7abd989e99cad917e65a39731
Israel Menon <zezomenon@gmail.com>
2020-08-27 19:03:09 -0300

    Exercicio 5 triangulo true ou false

---
+++ Introducao-Desenvolvimento-Web/Bloco_4/Dia_1/tresangulos.js
@@ -0,0 +1,11 @@
+let anguloInterno1 = 50;
+let anguloInterno2 = 100;
+let anguloInterno3 = 30;
+
+let somaAngulosInternos = anguloInterno1 + anguloInterno2 + anguloInterno3;
+
+if (somaAngulosInternos == 180) {
+  console.log("true")
+} else {
+  console.log("false - Não é um triângulo!")
+}
