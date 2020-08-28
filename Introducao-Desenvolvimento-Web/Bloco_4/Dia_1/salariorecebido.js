2ae67deacbff83518f8f8c28c018c29639d49519
Israel Menon <zezomenon@gmail.com>
2020-08-28 13:43:21 -0300

    Exercio 11 - está imcompleto, fala terminar... fazendo commit apenas pra deixar repositorio pronto para o dia 2 do bloco 4

---
+++ Introducao-Desenvolvimento-Web/Bloco_4/Dia_1/salariorecebido.js
@@ -0,0 +1,10 @@
+let salarioBruto = 3000;
+
+
+let salarioBase = salarioBruto - (salarioBruto * 0.11);
+console.log(salarioBase);
+let salarioLiquido = salarioBase - ((salarioBase * 0.075) - 142.80);
+console.log(salarioLiquido);
+
+// EXERCICIO INCOMPLETO!!!! 
+// PRECISA TERMINAR, ATÉ AGORA O QUE FOI FEITO FOI APENAS PRA PEGAR A IDEIA DO CÁCULO
