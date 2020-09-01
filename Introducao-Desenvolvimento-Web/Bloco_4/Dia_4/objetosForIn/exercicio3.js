b1de6046a8dcb13314874e5f0e80dd5c3759d0fb
Israel Menon <zezomenon@gmail.com>
2020-09-01 18:39:16 -0300

    Exercicio 3 parte I, utilizando for in para acessar as chaves do objeto

---
+++ Introducao-Desenvolvimento-Web/Bloco_4/Dia_4/objetosForIn/exercicio3.js
@@ -0,0 +1,10 @@
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
+info.recorrente = "Sim";
+
+for (i in info) {
+  console.log(i);
+}
