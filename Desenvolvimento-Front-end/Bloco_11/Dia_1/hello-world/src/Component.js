8f4afac6ea2bdc5f11de3585f8ff3c2391d6ebec
Israel Menon <zezomenon@gmail.com>
2020-11-05 16:30:34 -0300

    Iniciando novo módulo - Front-end - e criando primeiro ex de teste do dia (Hello, World)

---
+++ Desenvolvimento-Front-end/Bloco_11/Dia_1/hello-world/src/Component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ComponentJs extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Israel Menon Lisboa</h1>
+        <p>Futuro Front-end Web Developer</p>
+      </div>
+    );
+  }
+}
+
+export default ComponentJs;
